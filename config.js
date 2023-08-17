cf838af22f7d3eaa3ecd821fe722a5a40499c0bf
nagatani <22266171+inatagan@users.noreply.github.com>
2023-08-17 09:05:09 -0300

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
+	name: 'w̸͔̭͛̌͋͊͊́̾o̶͎̠̓̊̉̿̔͆̋̅͝ȓ̷̩̥̜̮̮͉͙̉̉̆͆͘͜ͅl̴̬̺͈͇̖̙̿̃̚͝ͅd̴͔̊̌̍̇̊͘',
 	imageBackground: false,
 	openInNewTab: true,
 	twelveHourFormat: false,
@@ -55,7 +55,7 @@ const CONFIG = {
 			id: '1',
 			name: 'Github',
 			icon: 'github',
-			link: 'https://github.com/',
+			link: 'https://github.com/inatagan',
 		},
 		{
 			id: '2',
